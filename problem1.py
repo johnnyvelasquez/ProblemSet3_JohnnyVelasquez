00285f4a38b404cdc978c879dc28c6aeaccb34d0
Johnny Velasquez <johnny.velasquez@login1.ufhpc>
2020-10-11 21:53:17 -0400

    problem one solved

--- problem1.py
+++ problem1.py
@@ -4,9 +4,19 @@
 #Open a file
 filename = '/blue/bsc4452/share/Class_Files/data/CO-OPS__8729108__wl.csv'
 file = open(filename, 'r')
+#Read each line in the csv file
+max_waterlevel = 0
 for line in file:
-	extracted_line = file.readline()
-	print(extracted_line)
-#Read each line
-#Report the highest water level and the date and time that it was observed
+	#Split the line by delimiter and store date/time and waterlevel in variables.
+	array = line.split(',')
+	date_time = array[0]
+	waterlevel = array[1]
+	if (max_waterlevel < waterlevel):
+		max_waterlevel = waterlevel
+		date_time = array[0]
+print("The max water level is", max_waterlevel)
+print("It was observed on", date_time)
+
+	
+
 
